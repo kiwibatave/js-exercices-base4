84ea0418e63707264974e2b2316d5f6a6b883a03
kiwibatave <b_cuzon@yahoo.fr>
2017-05-30 16:53:33 +0200

    premier jet

---
+++ question.js
@@ -0,0 +1,6 @@
+var questions = [
+  new Question("Quelle est la taille moyenne d'une banane?", ["5cm", "8cm", "12cm", "La réponse D"], "12cm"),
+  new Question("Quelle est la taille moyenne d'une banane?", ["5cm", "8cm", "12cm", "La réponse D"], "12cm"),
+  new Question("Quelle est la taille moyenne d'une banane?", ["5cm", "8cm", "12cm", "La réponse D"], "12cm"),
+  new Question("Quelle est la taille moyenne d'une banane?", ["5cm", "8cm", "12cm", "La réponse D"], "12cm")    
+]
